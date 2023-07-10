10af9e09b499146530b2e0d7d7d9d34b486e3f1d
Ahmed Abusweireh <82058235+retenuphilistia13@users.noreply.github.com>
2023-07-10 17:20:41 +0300

    _subject to change interpetaion for creating tabs

--- src/main/java/org/programmerhelper/ProgrammerHelper.java
+++ src/main/java/org/programmerhelper/ProgrammerHelper.java
@@ -55,6 +55,9 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
     private OutputManager<Boolean> previewManager, multipleManager;
     //private OutputManager<ActionEvent> actionManager;
     private JTabbedPane tabbedPane;
+
+   private int tabIndexJava =-1;
+    private int tabIndexCPlusPlus =-1;
     public ProgrammerHelper() {
         // Set up the frame
         setTitle("Programmer Little Helper");
@@ -249,18 +252,20 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
         javaSnipList = new ArrayList<>();
         CPlusPlusSnipList = new ArrayList<>();
 
-        if (javaItem.isSelected() && javaSnipList.isEmpty() && !flagFirstActivity) {
+        if (javaItem.isSelected()) {// && javaSnipList.isEmpty() && !flagFirstActivity
+
             OOPLanguage = new Java();
             isSnip = false;
 
             JavaSnippets javaSnip = new JavaSnippets(OOPLanguage, this, input, multi, livePrev);
             javaSnipList.add(javaSnip);
 
+            tabIndexJava +=1;
             createSnippet();
 
-            addNewTab(javaSnipList.get(0));
+            addNewTab(javaSnipList.get(tabIndexJava));
         }
-        else if (cPlusPlusItem.isSelected() && CPlusPlusSnipList == null && !flagFirstActivity) {
+        else if (cPlusPlusItem.isSelected()) {// && CPlusPlusSnipList.isEmpty() && !flagFirstActivity
 
 
             OOPLanguage = new C_Plus_Plus();
@@ -269,11 +274,11 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
             C_Plus_PlusSnippets CPlusPlusSnip = new C_Plus_PlusSnippets(OOPLanguage, this, input, multi, livePrev);
             CPlusPlusSnipList.add(CPlusPlusSnip);
 
+            tabIndexCPlusPlus +=1;
             createSnippet();
 
-            addNewTab(CPlusPlusSnipList.get(0));
+            addNewTab(CPlusPlusSnipList.get(tabIndexCPlusPlus));
 
-            //addToFrame();
         }
 
 
@@ -282,32 +287,6 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
         getContentPane().add(tabbedPane, BorderLayout.CENTER);
 
         setVisible(true);
-    }
-
-    static class CloseableTabComponent extends JPanel {
-        private final JTabbedPane tabbedPane;
-        private final int tabIndex;
-
-        public CloseableTabComponent(JTabbedPane tabbedPane, int tabIndex) {
-            super(new BorderLayout());
-            this.tabbedPane = tabbedPane;
-            this.tabIndex = tabIndex;
-            setOpaque(false);
-
-            // Create label for tab title
-            JLabel titleLabel = new JLabel(tabbedPane.getTitleAt(tabIndex));
-            add(titleLabel, BorderLayout.CENTER);
-
-            // Create close button
-
-            JButton closeButton = new JButton("X");
-            closeButton.setPreferredSize(new Dimension(12, 12));
-            //closeButton.setIcon(new ImageIcon("close_icon.png")); // Replace "close_icon.png" with your actual icon path
-            closeButton.setMargin(new Insets(0, 0, 0, 0));
-
-            closeButton.addActionListener(e -> tabbedPane.removeTabAt(tabIndex));
-            add(closeButton, BorderLayout.LINE_END);
-        }
     }
 
     //main
@@ -367,9 +346,14 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
 
             addCurrentAction();
 
-            updateTab();
+//            updateTab();
+            JavaSnippets javaSnip = new JavaSnippets(OOPLanguage, this, input, multi, livePrev);
+            javaSnipList.add(javaSnip);
 
+            tabIndexJava +=1;
             createSnippet();
+
+            addNewTab(javaSnipList.get(tabIndexJava));
 
             System.out.println("language "+language+"prev language"+prevLanguage);
 
@@ -385,9 +369,16 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
             language = OOPLanguage.getLanguageType();
 
             addCurrentAction();
-            updateTab();
 
+            C_Plus_PlusSnippets CPlusPlus = new C_Plus_PlusSnippets(OOPLanguage, this, input, multi, livePrev);
+            CPlusPlusSnipList.add(CPlusPlus);
+
+            tabIndexCPlusPlus +=1;
             createSnippet();
+
+            addNewTab(CPlusPlusSnipList.get(tabIndexCPlusPlus));
+
+            System.out.println("language "+language+"prev language"+prevLanguage);
 
             sendOOPPreferences(language);
 
@@ -456,19 +447,29 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
 
 
     private void addNewTab(JPanel panel) {
-        tabbedPane.addTab("tabTitle", panel);
-        for (int i = 0; i < tabbedPane.getTabCount(); i++) {
-            tabbedPane.setTabComponentAt(i, new CloseableTabComponent(tabbedPane, i));
+//        if(flagFirstActivity)
+//        tabIndex+=1;
+        tabbedPane.addTab("tabTitle "+ tabLangaugeIndex() +" "+language.name(), panel);
+        setClosableTabs();
+
+    }
+
+    private int tabLangaugeIndex(){
+
+        switch(language){
+            case JAVA -> {return tabIndexJava;}
+            case CPLUSPLUS -> {return tabIndexCPlusPlus;}
+            default -> {return tabIndexJava;}
         }
     }
     private void updateTab(){
         //tabbedPane.remove(removeFromFrame());
         removeFromFrame();
         removeSelectedTab();
-        tabbedPane.add("update tap",addOOPSnip(language));
-        for (int i = 0; i < tabbedPane.getTabCount(); i++) {
-            tabbedPane.setTabComponentAt(i, new CloseableTabComponent(tabbedPane, i));
-        }
+
+        tabbedPane.add("update tap "+language+" "+ tabLangaugeIndex(),addOOPSnip(language));
+
+        setClosableTabs();
     }
     private void removeSelectedTab() {
         int selectedIndex = tabbedPane.getSelectedIndex();
@@ -476,24 +477,28 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
             tabbedPane.removeTabAt(selectedIndex);
         }
     }
-
+private void setClosableTabs(){
+    for (int i = 0; i < tabbedPane.getTabCount(); i++) {
+        tabbedPane.setTabComponentAt(i, new CloseableTabComponent(tabbedPane, i));
+    }
+}
     public void sendOOPPreferences(Language language1) {
         switch (language1) {
             case JAVA:
                 if (!javaSnipList.isEmpty()) {
-                    javaSnipList.get(0).setOutput(outputManager.getUndoStack());
-                    javaSnipList.get(0).setUserInput(inputManager.getUndoStack());
-                    javaSnipList.get(0).setMultipleInputs(multipleManager.getUndoStack());
-                    javaSnipList.get(0).setPrevBox(previewManager.getUndoStack());
+                    javaSnipList.get(tabIndexJava).setOutput(outputManager.getUndoStack());
+                    javaSnipList.get(tabIndexJava).setUserInput(inputManager.getUndoStack());
+                    javaSnipList.get(tabIndexJava).setMultipleInputs(multipleManager.getUndoStack());
+                    javaSnipList.get(tabIndexJava).setPrevBox(previewManager.getUndoStack());
 
                 }
                 break;
             case CPLUSPLUS:
                 if (!CPlusPlusSnipList.isEmpty()) {
-                    CPlusPlusSnipList.get(0).setUserInput(inputManager.getUndoStack());
-                    CPlusPlusSnipList.get(0).setOutput(outputManager.getUndoStack());
-                    CPlusPlusSnipList.get(0).setPrevBox(previewManager.getUndoStack());
-                    CPlusPlusSnipList.get(0).setMultipleInputs(multipleManager.getUndoStack());
+                    CPlusPlusSnipList.get(tabIndexCPlusPlus).setUserInput(inputManager.getUndoStack());
+                    CPlusPlusSnipList.get(tabIndexCPlusPlus).setOutput(outputManager.getUndoStack());
+                    CPlusPlusSnipList.get(tabIndexCPlusPlus).setPrevBox(previewManager.getUndoStack());
+                    CPlusPlusSnipList.get(tabIndexCPlusPlus).setMultipleInputs(multipleManager.getUndoStack());
 
                 }
                 break;
@@ -507,17 +512,17 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
                 System.out.println("java initializing");
                 JavaSnippets javaSnip = new JavaSnippets(OOPLanguage, this, input, multi, livePrev);
                 javaSnipList.add(javaSnip);
-                return javaSnipList.get(0);
+                return javaSnipList.get(tabIndexJava);
             case CPLUSPLUS:
                 System.out.println("c++ initializing");
                 C_Plus_PlusSnippets CPlusPlusSnip = new C_Plus_PlusSnippets(OOPLanguage, this, input, multi, livePrev);
                 CPlusPlusSnipList.add(CPlusPlusSnip);
-                return CPlusPlusSnipList.get(0);
+                return CPlusPlusSnipList.get(tabIndexCPlusPlus);
             default:
                 System.out.println("java initializing (default)");
                 JavaSnippets javaSnipt = new JavaSnippets(OOPLanguage, this, input, multi, livePrev);
                 javaSnipList.add(javaSnipt);
-                return javaSnipList.get(0);
+                return javaSnipList.get(tabIndexJava);
         }
     }
 
@@ -543,20 +548,20 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
         switch (language) {
             case JAVA -> {
                 if (!javaSnipList.isEmpty()) {
-                    removedPanel = javaSnipList.remove(0);
+                    removedPanel = javaSnipList.remove(tabIndexJava);
                     remove(removedPanel);
                 }
             }
             case CPLUSPLUS-> {
                 if (!CPlusPlusSnipList.isEmpty()) {
-                    removedPanel = CPlusPlusSnipList.remove(0);
+                    removedPanel = CPlusPlusSnipList.remove(tabIndexCPlusPlus);
                     remove(removedPanel);
                 }
             }
 
             default-> {
                 if (!javaSnipList.isEmpty()) {
-                    removedPanel = javaSnipList.remove(0);
+                    removedPanel = javaSnipList.remove(tabIndexJava);
                     remove(removedPanel);
                 }
             }
@@ -615,21 +620,46 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
     public void createSnippet() {
 
         switch (language) {
-            case JAVA -> {eventHandler.handleJavaSnippet(oopSnip,javaSnipList.get(0));}
-            case CPLUSPLUS -> {eventHandler.handleCPlusPlusSnippet(oopSnip,CPlusPlusSnipList.get(0));}
+            case JAVA -> {eventHandler.handleJavaSnippet(oopSnip,javaSnipList.get(tabIndexJava));}
+            case CPLUSPLUS -> {eventHandler.handleCPlusPlusSnippet(oopSnip,CPlusPlusSnipList.get(tabIndexCPlusPlus));}
+        }
+    }
+
+    public class CloseableTabComponent extends JPanel {
+
+        public CloseableTabComponent(JTabbedPane tabbedPane, int tabIndex) {
+            super(new BorderLayout());
+            setOpaque(false);
+
+            // Create label for tab title
+            JLabel titleLabel = new JLabel(tabbedPane.getTitleAt(tabIndex));
+            add(titleLabel, BorderLayout.CENTER);
+
+            // Create close button
+
+            JButton closeButton = new JButton("*");
+            closeButton.setPreferredSize(new Dimension(20, 15));
+            //closeButton.setIcon(new ImageIcon("close_icon.png")); // Replace "close_icon.png" with your actual icon path
+            closeButton.setMargin(new Insets(0, 0, 0, 0));
+
+            closeButton.addActionListener(e -> {
+                //tabbedPane.removeTabAt(tabIndex);
+                removeSelectedTab();
+            });
+            add(closeButton, BorderLayout.LINE_END);
         }
     }
 
     private String getCurrentOutput() {
         switch (prevLanguage) {
             case JAVA -> {
-                return javaSnipList.get(0).getTextPaneText();
+                return javaSnipList.get(tabIndexJava).getTextPaneText();
             }
             case CPLUSPLUS -> {
-                return CPlusPlusSnipList.get(0).getTextPaneText();
+                return CPlusPlusSnipList.get(tabIndexCPlusPlus).getTextPaneText();
             }
             default -> {
-                return javaSnipList.get(0).getTextPaneText();
+                return javaSnipList.get(tabIndexJava).getTextPaneText();
             }
         }
         //    output = javaSnip.getTextPaneText();
@@ -640,13 +670,13 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
     private String getCurrentInput(){
         switch (prevLanguage) {
             case JAVA -> {
-                return javaSnipList.get(0).getInputText();
+                return javaSnipList.get(tabIndexJava).getInputText();
             }
             case CPLUSPLUS -> {
-                return CPlusPlusSnipList.get(0).getInputText();
+                return CPlusPlusSnipList.get(tabIndexCPlusPlus).getInputText();
             }
             default -> {
-                return javaSnipList.get(0).getInputText();
+                return javaSnipList.get(tabIndexJava).getInputText();
             }
         }
     }
@@ -654,13 +684,13 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
     private boolean getCurrentPreview(){
         switch (prevLanguage) {
             case JAVA -> {
-                return javaSnipList.get(0).isLivePrev();
+                return javaSnipList.get(tabIndexJava).isLivePrev();
             }
             case CPLUSPLUS -> {
-                return CPlusPlusSnipList.get(0).isLivePrev();
+                return CPlusPlusSnipList.get(tabIndexCPlusPlus).isLivePrev();
             }
             default -> {
-                return javaSnipList.get(0).isLivePrev();
+                return javaSnipList.get(tabIndexJava).isLivePrev();
             }
         }
     }
@@ -668,13 +698,13 @@ public class ProgrammerHelper extends JFrame implements ActionListener, PanelLis
     private boolean getCurrentMultiInput(){
         switch (prevLanguage) {
             case JAVA -> {
-                return javaSnipList.get(0).isMultiInputs();
+                return javaSnipList.get(tabIndexJava).isMultiInputs();
             }
             case CPLUSPLUS -> {
-                return CPlusPlusSnipList.get(0).isMultiInputs();
+                return CPlusPlusSnipList.get(tabIndexCPlusPlus).isMultiInputs();
             }
             default -> {
-                return javaSnipList.get(0).isMultiInputs();
+                return javaSnipList.get(tabIndexJava).isMultiInputs();
             }
         }
     }
